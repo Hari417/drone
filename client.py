ea12ed5042ccecddc17de3b61aa2e2d10c8d3ae9
Hari417 <hariharansp2050@gmail.com>
2024-08-02 20:11:50 +0530

    allfiles

---
+++ client.py
@@ -0,0 +1,22 @@
+# client.py
+import cv2
+import socket
+import struct
+import pickle
+
+# Connect to the remote server
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('your_server_ip', 8485))  # Replace 'your_server_ip' with the actual server IP
+connection = client_socket.makefile('wb')
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
+    data = pickle.dumps(frame)
+    client_socket.sendall(struct.pack("L", len(data)) + data)
+
+cam.release()
+client_socket.close()
